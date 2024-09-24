6287f1f282a62fb544fc27cf9e1244a6cb75ac34
LiamHsu <s459958@gmail.com>
2024-09-24 09:33:47 +0800

    Init xcode project

---
+++ HomePlus/HomePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePlusApp.swift
+//  HomePlus
+//
+//  Created by Liam on 2024/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HomePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomePlusTests/HomePlusTests.swift
@@ -0,0 +1,16 @@
+//
+//  HomePlusTests.swift
+//  HomePlusTests
+//
+//  Created by Liam on 2024/9/24.
+//
+
+import Testing
+
+struct HomePlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
